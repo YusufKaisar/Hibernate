7b0f1ca8a440fa9d86c5f98b9c1b0d409515e1e4
YusufKaisar <128247503+YusufKaisar@users.noreply.github.com>
2024-06-19 14:09:36 +0700

    Update UAS_LAST_PBO.java

--- UAS_LAST_PBO/src/uas_last_pbo/UAS_LAST_PBO.java
+++ UAS_LAST_PBO/src/uas_last_pbo/UAS_LAST_PBO.java
@@ -194,15 +194,90 @@ public class UAS_LAST_PBO {
         course1.getScores().add(score3);
         session.save(score3);
 
-        Score score4 = new Score(70, student6, course2);
+        Score score4 = new Score(70, student6, course1);
         student6.getScores().add(score4);
-        course2.getScores().add(score4);
+        course1.getScores().add(score4);
         session.save(score4);
 
         Score score5 = new Score(90, student3, course1);
         student3.getScores().add(score5);
         course1.getScores().add(score5);
         session.save(score5);    
+
+        Score score6 = new Score(98, student4, course1);
+        student4.getScores().add(score6);
+        course1.getScores().add(score6);
+        session.save(score6);    
+
+        Score score7 = new Score(53, student5, course1);
+        student5.getScores().add(score7);
+        course1.getScores().add(score7);
+        session.save(score7);    
+
+        Score score8 = new Score(59, student6, course2);
+        student6.getScores().add(score8);
+        course2.getScores().add(score8);
+        session.save(score8);    
+
+        Score score9 = new Score(56, student7, course1);
+        student7.getScores().add(score9);
+        course1.getScores().add(score9);
+        session.save(score9);    
+
+        Score score10 = new Score(63, student7, course2);
+        student7.getScores().add(score10);
+        course2.getScores().add(score10);
+        session.save(score10);    
+
+        Score score11 = new Score(75, student8, course2);
+        student8.getScores().add(score11);
+        course2.getScores().add(score11);
+        session.save(score11);    
+
+        Score score12 = new Score(89, student9, course2);
+        student9.getScores().add(score12);
+        course2.getScores().add(score12);
+        session.save(score12);    
+
+        Score score13 = new Score(20, student10, course2);
+        student10.getScores().add(score13);
+        course2.getScores().add(score13);
+        session.save(score13);    
+
+        Score score14 = new Score(45, student11, course3);
+        student11.getScores().add(score14);
+        course3.getScores().add(score14);
+        session.save(score14);    
+
+        Score score15 = new Score(65, student12, course3);
+        student12.getScores().add(score15);
+        course3.getScores().add(score15);
+        session.save(score15);    
+
+        Score score16 = new Score(94, student13, course3);
+        student13.getScores().add(score16);
+        course3.getScores().add(score16);
+        session.save(score16);    
+
+        Score score17 = new Score(83, student14, course3);
+        student14.getScores().add(score17);
+        course3.getScores().add(score17);
+        session.save(score17);    
+
+        Score score18 = new Score(87, student15, course3);
+        student15.getScores().add(score18);
+        course4.getScores().add(score18);
+        session.save(score18);    
+
+        Score score19 = new Score(90, student16, course4);
+        student16.getScores().add(score19);
+        course4.getScores().add(score19);
+        session.save(score19);    
+
+        Score score20 = new Score(95, student17, course4);
+        student17.getScores().add(score20);
+        course4.getScores().add(score20);
+        session.save(score20);    
 
         transaction.commit();
 
@@ -227,10 +302,88 @@ public class UAS_LAST_PBO {
         System.out.println("| IPK " + ": " + student3.calculateGPA() + " |");
         System.out.println("---------------------------------------------------");
         
+        System.out.println("KHS " + student4.getUsernameId() + ":");
+        System.out.println("| Course Code |" + " Course Name |" + " Score |");
+        student4.printAllScores();
+        System.out.println("| IPK " + ": " + student4.calculateGPA() + " |");
+        System.out.println("---------------------------------------------------");
+        
+        System.out.println("KHS " + student5.getUsernameId() + ":");
+        System.out.println("| Course Code |" + " Course Name |" + " Score |");
+        student5.printAllScores();
+        System.out.println("| IPK " + ": " + student5.calculateGPA() + " |");
+        System.out.println("---------------------------------------------------");
+        
         System.out.println("KHS " + student6.getUsernameId() + ":");
         System.out.println("| Course Code |" + " Course Name |" + " Score |");
         student6.printAllScores();
         System.out.println("| IPK " + ": " + student6.calculateGPA() + " |");
+        System.out.println("---------------------------------------------------");
+        
+        System.out.println("KHS " + student7.getUsernameId() + ":");
+        System.out.println("| Course Code |" + " Course Name |" + " Score |");
+        student7.printAllScores();
+        System.out.println("| IPK " + ": " + student7.calculateGPA() + " |");
+        System.out.println("---------------------------------------------------");
+        
+        System.out.println("KHS " + student8.getUsernameId() + ":");
+        System.out.println("| Course Code |" + " Course Name |" + " Score |");
+        student8.printAllScores();
+        System.out.println("| IPK " + ": " + student8.calculateGPA() + " |");
+        System.out.println("---------------------------------------------------");
+        
+        System.out.println("KHS " + student9.getUsernameId() + ":");
+        System.out.println("| Course Code |" + " Course Name |" + " Score |");
+        student9.printAllScores();
+        System.out.println("| IPK " + ": " + student9.calculateGPA() + " |");
+        System.out.println("---------------------------------------------------");
+        
+        System.out.println("KHS " + student10.getUsernameId() + ":");
+        System.out.println("| Course Code |" + " Course Name |" + " Score |");
+        student10.printAllScores();
+        System.out.println("| IPK " + ": " + student10.calculateGPA() + " |");
+        System.out.println("---------------------------------------------------");
+        
+        System.out.println("KHS " + student11.getUsernameId() + ":");
+        System.out.println("| Course Code |" + " Course Name |" + " Score |");
+        student11.printAllScores();
+        System.out.println("| IPK " + ": " + student11.calculateGPA() + " |");
+        System.out.println("---------------------------------------------------");
+        
+        System.out.println("KHS " + student12.getUsernameId() + ":");
+        System.out.println("| Course Code |" + " Course Name |" + " Score |");
+        student12.printAllScores();
+        System.out.println("| IPK " + ": " + student12.calculateGPA() + " |");
+        System.out.println("---------------------------------------------------");
+        
+        System.out.println("KHS " + student13.getUsernameId() + ":");
+        System.out.println("| Course Code |" + " Course Name |" + " Score |");
+        student13.printAllScores();
+        System.out.println("| IPK " + ": " + student13.calculateGPA() + " |");
+        System.out.println("---------------------------------------------------");
+        
+        System.out.println("KHS " + student14.getUsernameId() + ":");
+        System.out.println("| Course Code |" + " Course Name |" + " Score |");
+        student14.printAllScores();
+        System.out.println("| IPK " + ": " + student14.calculateGPA() + " |");
+        System.out.println("---------------------------------------------------");
+        
+        System.out.println("KHS " + student15.getUsernameId() + ":");
+        System.out.println("| Course Code |" + " Course Name |" + " Score |");
+        student15.printAllScores();
+        System.out.println("| IPK " + ": " + student15.calculateGPA() + " |");
+        System.out.println("---------------------------------------------------");
+        
+        System.out.println("KHS " + student16.getUsernameId() + ":");
+        System.out.println("| Course Code |" + " Course Name |" + " Score |");
+        student16.printAllScores();
+        System.out.println("| IPK " + ": " + student16.calculateGPA() + " |");
+        System.out.println("---------------------------------------------------");
+        
+        System.out.println("KHS " + student17.getUsernameId() + ":");
+        System.out.println("| Course Code |" + " Course Name |" + " Score |");
+        student17.printAllScores();
+        System.out.println("| IPK " + ": " + student17.calculateGPA() + " |");
         System.out.println("---------------------------------------------------");
 
 
